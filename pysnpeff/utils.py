e5182f24279c6da1f6c4b8bbaa0542edeeb8cb64
ramonamela <ramon.amela@gmail.com>
2019-09-30 17:03:11 +0200

    State before changing laptop

--- pysnpeff/utils.py
+++ pysnpeff/utils.py
@@ -2,6 +2,7 @@
 import json
 from subprocess import Popen, PIPE
 import sys
+from jsonschema import validate, validators, Draft7Validator
 
 def _from_json_to_command(config):
     """Annotates the input_stream to the output_stream following the config
@@ -15,7 +16,28 @@ def _from_json_to_command(config):
     config : dict
         Dictionary describing the annotation step to be performed
     """
-    return "java -Xmx64g -jar /mnt/nfs/eucancan_vcf_vc/SnpSift/target/SnpSift-4.4.jar annotate -id -noInfo /mnt/nfs/eucancan_vcf_vc/databases/00-All.vcf.gz /dev/stdin -a"
+    return "java -Xmx64g -jar $(readlink -f $(which SnpSift.jar)) annotate -id -noInfo /home/ramela/Downloads/example_vcfs/databases/00-All.vcf.gz /dev/stdin -a"
+
+def _validate_with_default(validator_class):
+    validate_properties = validator_class.VALIDATORS["properties"]
+
+    def set_defaults(validator, properties, instance, schema):
+        for property, subschema in properties.items():
+            if "default" in subschema:
+                instance.setdefault(property, subschema["default"])
+
+        for error in validate_properties(
+            validator, properties, instance, schema,
+        ):
+            yield error
+
+    return validators.extend(
+        validator_class, {"properties" : set_defaults},
+    )
+
+def _check_json_file(input_json, json_schema):
+    DefaultValidatingDraft7Validator = _validate_with_default(Draft7Validator)
+    DefaultValidatingDraft7Validator(json_schema).validate(input_json)
 
 def annotate(input_file, output_file, json_file):
     """Annotates the input_file to the output_file following the configuration
@@ -30,21 +52,29 @@ def annotate(input_file, output_file, json_file):
         Dictionary containing the necessary information to perform the
         annotation
     """
-    config_json = json.load(json_file)
-    out_stream = open(input_file, "r")
-    for i in range(config_json["steps"]):
-        command = str.split(_from_json_to_command(config_json["steps"][i]))
+
+    with open(json_file, 'r') as f:
+        config_json = json.load(f)
+
+    with open("/home/ramela/git/PySnpEff/pysnpeff/config/schema.json") as f:
+        schema_json = json.load(f)
+
+    _check_json_file(config_json, schema_json)
+
+    p = Popen(["bash", "-c", "zcat " + input_file], stdout=PIPE)
+    out_stream = p.stdout
+    for i in range(len(config_json["steps"])):
+        command = ["bash", "-c", _from_json_to_command(config_json["steps"][i])]
         in_stream = out_stream
-        if i == len(config_json["steps"] - 1):
-            out_stream = open(output_file, "rw")
-        else:
-            out_stream = PIPE
-        p = Popen(command, stdin=in_stream, stdout=out_stream)
+        p = Popen(command, stdin=in_stream, stdout=PIPE)
         out_stream = p.stdout
+    in_stream = out_stream
+    out_stream = open(output_file, "w")
+    p = Popen(["bash", "-c", "bgzip"], stdin=in_stream, stdout=out_stream)
     p.wait()
 
 def main(in_file, out_file, json_file):
-    annotate(in_file, out_file, json_file)
+    return annotate(in_file, out_file, json_file)
 
 if __name__ == "__main__":
     in_file = sys.argv[1]
